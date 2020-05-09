0f490072070f03eb996e9aea39cb95ae0d482562
zampasoft <zampasoft@users.noreply.github.com>
2020-05-09 19:15:49 +0200

    prova

--- pippo.py
+++ pippo.py
@@ -1,32 +1,34 @@
 # loading the class data from the package pandas_datareader
-from typing import List
+# from typing import List
 
 from pandas_datareader import data
 
+
 # Asset Classes that I focus on
 class AssetClass:
-    def __init__(self, assetType: str, buyCommission: float, annualFee: float) -> object:
+    def __init__(self, asset_type: str, buy_commission: float, annual_fee: float) -> object:
         """
 
-        :type buyCommission: float
+        :type buy_commission: float
         """
         AssetClasses = ("equity", "ETC", "ETF", "currency")
-        if assetType in AssetClasses:
-            self.assetType = assetType
-            self.hadDividends =  assetType in ("equity", "ETF")
+        if asset_type in AssetClasses:
+            self.assetType = asset_type
+            self.hadDividends = asset_type in ("equity", "ETF")
         else:
             raise ValueError
-        if 0 <= buyCommission < 1:
-            self.buyCommission = buyCommission
+        if 0 <= buy_commission < 1:
+            self.buyCommission = buy_commission
         else:
             raise ValueError
-        if 0 <= annualFee < 1:
-            self.annualFee = annualFee
+        if 0 <= annual_fee < 1:
+            self.annualFee = annual_fee
         else:
             raise ValueError
 
     def hasDividends(self):
         return self.hadDividends
+
 
 #
 Equity = AssetClass("equity", 0.003, 0)
@@ -63,15 +65,16 @@ end_date = '2019-06-06'
 # Call the function DataReader from the class data
 ## amp_data = data.DataReader('AMP.MI', 'yahoo', start_date, end_date)
 ## data = data.DataReader('GBPEUR=X', 'yahoo', start_date, end_date)
-amp_quo = data.DataReader('AMP.MI', "yahoo", start_date, end_date) ['Adj Close']
-amp_act = data.DataReader('AMP.MI', "yahoo-actions", start_date, end_date)
-amp_div = data.DataReader('AMP.MI', "yahoo-dividends", start_date, end_date)
-#necessario trovare DataReader per Dividendi
+#amp_quo = data.DataReader("AMP.MI", "yahoo", start_date, end_date)['Adj Close']
+amp_quo = data.DataReader("AMP.MI", "yahoo", start_date, end_date)
+amp_act = data.DataReader("AMP.MI", "yahoo-actions", start_date, end_date)
+amp_div = data.DataReader("AMP.MI", "yahoo-dividends", start_date, end_date)
+# necessario trovare DataReader per Dividendi
 
-import pandas as pd
+# import pandas as pd
 
 # pd.set_printoptions(max_colwidth, 1000)
-pd.set_option('display.width', 1000)
+# pd.set_option('display.width', 1000)
 
 print()
 print(amp_div)
